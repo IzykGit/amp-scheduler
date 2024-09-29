632cecfa23e2f7aade00c90173ce9eb137a21f68
Lance Hemphill <lance_hemphill1234@yahoo.com>
2024-09-29 02:09:34 -0400

    made nav and fold

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -8,10 +8,26 @@ const Home = () => {
     return (
         <>
         <Navbar />
+
         <main className={HomeStyles.home_main}>
 
             <section className={HomeStyles.fold}>
 
+                <div className={HomeStyles.fold_text}>
+                    <h1 className={HomeStyles.fold_slogan}>We Make Scheduling Easy</h1>
+
+                    <ul className={HomeStyles.fold_list}>
+                        <li>Create Schedules</li>
+                        <li>Host Meetings</li>
+                        <li>Manage Employees</li>
+                    </ul>
+
+                </div>
+
+            </section>
+
+            <section>
+                
             </section>
 
         </main>
